cbfcd0568bb8ec73e1f813949548d1d881842a09
nashvail <nishantve1@gmail.com>
2016-05-15 18:02:27 +0530

    Navigation bar highlights on appropriate sections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,173 @@
-console.log('This will soon have something');
+// Page Section object 
+
+function Section(identifer) {
+	this.id = identifer;
+	this.el = document.querySelector('.section--' + identifer);
+	this.startPos = 0;
+}
+
+Section.prototype = {
+
+	height: function() {
+		return this.el.getBoundingClientRect().height;
+	}
+
+};
+
+// All The sections
+var sections = {
+	home: new Section('home'),
+	about: new Section('about'),
+	// faq: new Section('faq'),
+	// schedule: new Section('schedule'),
+	// sponsors: new Section('sponsors')
+};
+
+
+var navigationBarHeight = 90,
+	navigationBarInitialPos = sections.home.height() - navigationBarHeight;
+
+initSectionStartPositions();
+
+function initSectionStartPositions() {
+	sections.home.startPos = 0;
+	sections.about.startPos = sections.home.height() - navigationBarHeight;
+	// sections.faq.startPos = sections.about.height() - navigationBarHeight;
+	// sections.schedule.startPos = sections.faq.height() - navigationBarHeight;
+	// sections.sponsors.startPos = sections.schedule.height() - navigationBarHeightl;
+}
+
+// Navigation block Object 
+function Nav(id) {
+	this.id = id;
+	this.el = document.getElementById(id);
+	this.isActive = false;
+
+	this.events.click.call(this);
+}
+
+Nav.prototype = {
+	highlight: function() {
+		if(!this.isActive){
+			this.el.classList.remove('navigation__nav');
+			this.el.classList.add('navigation__nav--active');
+			this.isActive = true;
+		}
+	},
+
+	unhighlight: function() {
+		if(this.isActive) {
+			this.el.classList.remove('navigation__nav--active');
+			this.el.classList.add('navigation__nav');
+			this.isActive = false;	
+		}
+	},
+
+	events: {
+		click: function() {
+			this.el.addEventListener('click', function() {
+				console.log(this.id);	
+			});
+		}
+	}
+}
+
+// Navigation Bar 
+var navigationBar = {
+
+	el: document.querySelector('.navigation'),
+	stuckAtTop: false,
+
+	navs: {
+		home: new Nav('home'), 
+		about: new Nav('about'), 
+		faq: new Nav('faq'), 
+		schedule: new Nav('schedule'), 
+		sponsors: new Nav('sponsors')
+	},
+
+	// Navigation Bar functions 
+
+	positionAtIntialPos: function() {
+		this.el.style.top = navigationBarInitialPos + 'px';
+		this.navs.home.highlight();
+	},
+
+	shouldStick: function() {
+		var scrollPos = (document.documentElement && document.documentElement.scrollTop || document.body.scrollTop);
+		return(scrollPos >= navigationBarInitialPos);
+	},
+
+	shouldNotStick: function() {
+		var scrollPos = (document.documentElement && document.documentElement.scrollTop || document.body.scrollTop);
+		return(scrollPos < navigationBarInitialPos);
+	},
+
+	sitckToTop() {
+		if( !this.stuckAtTop ) {
+			this.el.style.position = 'fixed';
+			this.el.style.top = '0px';
+			this.stuckAtTop = true;
+		}	
+	},
+
+	unstick() {
+		if( this.stuckAtTop ) {
+			this.el.style.position = 'absolute';
+			this.positionAtIntialPos();
+			this.stuckAtTop = false;	
+		}
+	},
+
+	highlightNav: function(identifier) {
+		for (var nav in this.navs){
+			if (nav === identifier)
+				this.navs[nav].highlight();
+			else
+				this.navs[nav].unhighlight();
+		}
+	}
+
+};
+
+function highlightNavBlock() {
+	if( inSection('home')) {
+		navigationBar.highlightNav('home');
+	} else if ( inSection('about') ) {
+		navigationBar.highlightNav('about');
+	} 
+
+}
+
+function inSection(section) {
+	var scrollPos = (document.documentElement && document.documentElement.scrollTop || document.body.scrollTop);
+	var lowerLimit = sections[section].startPos;
+	var upperLimit = lowerLimit + sections[section].height() - navigationBarHeight;
+
+	return scrollPos >= lowerLimit && scrollPos < upperLimit;
+}
+
+
+
+navigationBar.positionAtIntialPos();
+
+// Event Listeners 
+
+window.addEventListener('resize', function() {
+	navigationBarInitialPos = sections.home.height() - navigationBarHeight;
+	navigationBar.positionAtIntialPos();
+	initSectionStartPositions();
+});
+
+window.addEventListener('scroll', function() {
+
+	if( navigationBar.shouldStick() ) {
+		navigationBar.sitckToTop();
+	} else if ( navigationBar.shouldNotStick() ) {
+		navigationBar.unstick();
+	}
+
+	highlightNavBlock();
+
+});
+
