e33098c4b13f80084b4752f38e9810cb263d8a40
nashvail <nishantve1@gmail.com>
2016-05-16 23:00:28 +0530

    Fixed accordion one opening 2nd panel

--- js/bundle.js
+++ js/bundle.js
@@ -71,19 +71,21 @@
 	var navigationBarHeight = 90;
 	var navigationBarInitialPos = sections.home.height() - navigationBarHeight;
 
-	// Event Listeners
-	window.addEventListener("DOMContentLoaded", function (event) {
+	$(window).load(function () {
 		navigationBar.positionAtIntialPos(navigationBarInitialPos);
 		initSectionStartPositions();
-
-		// Accordion
-		$('.collapse.in').prev('.panel-heading').addClass('active');
-		$('#accordion, #bs-collapse').on('show.bs.collapse', function (a) {
-			$(a.target).prev('.panel-heading').addClass('active');
-		}).on('hide.bs.collapse', function (a) {
-			$(a.target).prev('.panel-heading').removeClass('active');
-		});
 	});
+
+	$('.collapse.in').prev('.panel-heading').addClass('active');
+	$('#accordion, #bs-collapse').on('show.bs.collapse', function (a) {
+		$(a.target).prev('.panel-heading').addClass('active');
+	}).on('hide.bs.collapse', function (a) {
+		$(a.target).prev('.panel-heading').removeClass('active');
+	});
+
+	// Event Listeners
+	// window.addEventListener("DOMContentLoaded", function(event) {
+	// });
 
 	window.addEventListener('resize', function () {
 		initSectionStartPositions();
